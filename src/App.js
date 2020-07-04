1f5c19bb1ce9177728db1944d7a6091aa2560b4a
wangkaiwd <1162486176@qq.com>
2020-07-04 14:47:22 +0800

    feat: complement rich text editor try out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import RichTextEditor from './components/RichTextEditor';
+import BraftEditor from 'braft-editor';
 
-function App () {
-  return (
-    <div className="App">
-      <RichTextEditor/>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    editorState: BraftEditor.createEditorState('<p>hello</p>'),
+  };
+  onChange = (editorState) => {
+    this.setState({ editorState });
+  };
+
+  render () {
+    const { editorState } = this.state;
+    return (
+      <div className="App">
+        <RichTextEditor
+          className={'xxx'}
+          value={editorState}
+          onChange={this.onChange}
+        />
+        <button onClick={() => {console.log(editorState.toHTML());}}>get html</button>
+        <button onClick={() => {
+          this.setState({
+            editorState: BraftEditor.createEditorState('<p>default value</p>')
+          });
+        }}>set html
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
